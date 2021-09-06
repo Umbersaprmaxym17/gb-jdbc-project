0c5eb9682e9d7bd4a0caa360eab5e55fecbd8916
Maxym Saprykin <maxym.saprykin@hypr.com>
2021-09-05 22:08:25 -0400

    add

--- src/main/java/com/max/JdbcApp.java
+++ src/main/java/com/max/JdbcApp.java
@@ -3,9 +3,11 @@ package com.max;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class JdbcApp {
     private Connection connection;
+    private Statement statement;
 
 
 
@@ -13,6 +15,7 @@ public class JdbcApp {
         final JdbcApp jdbcApp = new JdbcApp();
         try {
             jdbcApp.connect();
+            jdbcApp.createTable();
         } catch (SQLException e) {
             e.printStackTrace();
         } finally {
@@ -22,13 +25,35 @@ public class JdbcApp {
 
     }
 
+    private void createTable() throws SQLException {
+        statement.executeUpdate("CREATE TABLE IF NOT EXISTS students (" +
+                "id INTEGER PRIMARY KEY AUTOINCREMENT," +
+                "name TEXT," +
+                "score INTEGER" +
+                ");");
+    }
+
+
+
+
+
+
+
 
 
     private void connect() throws SQLException {
         connection = DriverManager.getConnection("jdbc:sqlite:javadb.db");
+        statement = connection.createStatement();
     }
 
     private void disconnect() {
+        if (statement !=null) {
+            try {
+                statement.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
         if (connection !=null) {
             try {
                 connection.close();
@@ -37,4 +62,5 @@ public class JdbcApp {
             }
         }
     }
+
 }
